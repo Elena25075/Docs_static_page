3b7d95994a78357baec406776828602928661a4c
Elena Kartoshkina <96824172+Elena25075@users.noreply.github.com>
2025-05-28 19:28:57 +0300

    Update next.config.js
    More simple version added

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
-// next.config.js
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [require('remark-gfm')],
-    rehypePlugins: [require('rehype-highlight'), require('rehype-slug')],
-  },
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  experimental: {
-    mdxRs: false,
-  }
+  distDir: 'out',
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
